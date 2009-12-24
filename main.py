bcfd8aa6d1a313746b27545aa60413e3d1183c88
wasauce <wferrell@gmail.com>
2009-12-23 18:39:27 -0700

    Refixing back to ('/error', SimplePageHandler), #simple.html

--- main.py
+++ main.py
@@ -330,7 +330,7 @@ _MountainMetrics_Urls = [
 # after each URL map we list the html template that is displayed
 #   ('/bill', Bill), #base.html
    ('/error', ErrorPageHandler), #error.html
-   ('/simple', SimplePageHandler), #simple.html
+   ('/error', SimplePageHandler), #simple.html
    ('/about', AboutPageHandler), #about.html
    ('/.*$', HomePageHandler), #base.html
 ]
