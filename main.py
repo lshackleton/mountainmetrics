80a3be6bc474daebd8018c08b92ad85acace62f0
Bill Ferrell <wferrell@gmail.com>
2009-12-27 00:11:35 -0800

    I have refactored how we call data to abstract it into a cleaner, simpler function. I then inside that single function have re-written the timestamps to PST.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ logging.getLogger().setLevel(logging.INFO)
 template.register_template_library('templatefilters')
 
 # Set to true to see stack traces and template debugging information
-_DEBUG = True
+_DEBUG = False
 
 
 class BaseRequestHandler(webapp.RequestHandler):
@@ -120,28 +120,26 @@ class HomePageHandler(BaseRequestHandler):
 
   def get(self, garbageinput=None):
     logging.info('Visiting the homepage')
-    
-    weather = memcache.get("weather")
-    if weather is None:
-      weather_query = models.ThreeDayWeatherForecast.all()
-      weather_query.order('-date_time_added')
-      weather = weather_query.get()
-      memcache.set("weather", weather, time=3600)
 
-    roads = memcache.get("roads")
-    if roads is None:
-      road_query = models.DOTi80RoadConditions.all()
-      road_query.order('-date_time_added')
-      roads = road_query.get()
-      memcache.set("roads", roads, time=3600)
+    handler = mmutil.DataPopulator()
+    weather = handler.handle(type='weather')
+    roads = handler.handle(type='roads')
+    avalanche = handler.handle(type='avalanche')
+    alpine_meadows_snow_report = handler.handle(type=
+                                                'alpine_meadows_snow_report')
+    squaw_valley_snow_report = handler.handle(type='squaw_valley_snow_report')
+    kirkwood_snow_report = handler.handle(type='kirkwood_snow_report')
+    expected_snowfall = handler.handle(type='expected_snowfall')
+    yahoo_weather = handler.handle(type='yahoo_weather')
+    current_obs = handler.handle(type='current_obs')
+    sierra_weather = handler.handle(type='sierra_weather')
+    sierra_temp = handler.handle(type='sierra_temp')
+    sierra_wind_direction = handler.handle(type='sierra_wind_direction')
+    sierra_wind_speed = handler.handle(type='sierra_wind_speed')
+    sierra_expected_snow = handler.handle(type='sierra_expected_snow')
+    yesterday_data = handler.handle(type='yesterday_data')             
 
-    avalanche = memcache.get("avalanche")
-    if avalanche is None:
-      avalanche_query = models.TodaysAvalancheReport.all()
-      avalanche_query.order('-date_time_added')
-      avalanche = avalanche_query.get()
-      memcache.set("avalanche", avalanche, time=3600)
-    
+  
     avalanche_multi_levels = False  
     if avalanche.multiple_danger_levels:
       avalanche_multi_levels = True
@@ -164,110 +162,6 @@ class HomePageHandler(BaseRequestHandler):
       avalanche_graph_url = 'http://chart.apis.google.com/chart?cht=gom&chs=400x200&chd=t:90&chl=Low&chdlp=b'
       avalanche_status = 'Low'
 
-    alpine_meadows_snow_report = memcache.get("alpine_meadows_snow_report")
-    if alpine_meadows_snow_report is None:
-      alpine_meadows_snow_report_query = models.AlpineMeadowsSnowReport.all()
-      alpine_meadows_snow_report_query.order('-date_time_added')
-      alpine_meadows_snow_report = alpine_meadows_snow_report_query.get()
-      memcache.set("alpine_meadows_snow_report", alpine_meadows_snow_report,
-                   time=3600)
-
-    squaw_valley_snow_report = memcache.get("squaw_valley_snow_report")
-    if squaw_valley_snow_report is None:
-      squaw_valley_snow_report_query = models.SquawValleySnowReport.all()
-      squaw_valley_snow_report_query.order('-date_time_added')
-      squaw_valley_snow_report = squaw_valley_snow_report_query.get()
-      memcache.set("squaw_valley_snow_report", squaw_valley_snow_report,
-                   time=3600)
-
-    kirkwood_snow_report = memcache.get("kirkwood_snow_report")
-    if kirkwood_snow_report is None:
-      kirkwood_snow_report_query = models.KirkwoodSnowReport.all()
-      kirkwood_snow_report_query.order('-date_time_added')
-      kirkwood_snow_report = kirkwood_snow_report_query.get()
-      memcache.set("kirkwood_snow_report", kirkwood_snow_report,
-                   time=3600)
-
-    expected_snowfall = memcache.get("expected_snowfall")
-    if expected_snowfall is None:
-      expected_snowfall_query = models.ExpectedSnowfall.all()
-      expected_snowfall_query.order('-date_time_added')
-      expected_snowfall = expected_snowfall_query.get()
-      logging.info('expected_snowfall: %s' % str(expected_snowfall))
-      memcache.set("expected_snowfall", expected_snowfall,
-                   time=3600)
-
-    yahoo_weather = memcache.get("yahoo_weather")
-    if yahoo_weather is None:
-      yahoo_weather_query = models.YahooWeatherForecast.all()
-      yahoo_weather_query.order('-date_time_added')
-      yahoo_weather = yahoo_weather_query.get()
-      logging.info('yahoo_weather: %s' % str(yahoo_weather))
-      memcache.set("yahoo_weather", yahoo_weather,
-                   time=3600)
-
-    current_obs = memcache.get("current_observations")
-    if current_obs is None:
-      current_obs_query = models.SierraAvyCenterCurrentObservations.all()
-      current_obs_query.order('-date_time_added')
-      current_obs = current_obs_query.get()
-      logging.info('current_obs: %s' % str(current_obs))
-      memcache.set("current_obs", current_obs,
-                   time=3600)
-
-    sierra_weather = memcache.get("sierra_weather")
-    if sierra_weather is None:
-      sierra_weather_query = models.SierraAvyCenterWeather.all()
-      sierra_weather_query.order('-date_time_added')
-      sierra_weather = sierra_weather_query.get()
-      logging.info('sierra_weather: %s' % str(sierra_weather))
-      memcache.set("sierra_weather", sierra_weather,
-                   time=3600)
-
-    sierra_temp = memcache.get("sierra_temp")
-    if sierra_temp is None:
-      sierra_temp_query = models.SierraAvyCenterTemperatures.all()
-      sierra_temp_query.order('-date_time_added')
-      sierra_temp = sierra_temp_query.get()
-      logging.info('sierra_temp: %s' % str(sierra_temp))
-      memcache.set("sierra_temp", sierra_temp,
-                   time=3600)
-
-    sierra_wind_direction = memcache.get("sierra_wind_direction")
-    if sierra_wind_direction is None:
-      sierra_wind_direction_query = models.SierraAvyCenterWindDirection.all()
-      sierra_wind_direction_query.order('-date_time_added')
-      sierra_wind_direction = sierra_wind_direction_query.get()
-      logging.info('sierra_wind_direction: %s' % str(sierra_wind_direction))
-      memcache.set("sierra_wind_direction", sierra_wind_direction,
-                   time=3600)
-
-    sierra_wind_speed = memcache.get("sierra_wind_speed")
-    if sierra_wind_speed is None:
-      sierra_wind_speed_query = models.SierraAvyCenterWindSpeed.all()
-      sierra_wind_speed_query.order('-date_time_added')
-      sierra_wind_speed = sierra_wind_speed_query.get()
-      logging.info('sierra_wind_speed: %s' % str(sierra_wind_speed))
-      memcache.set("sierra_wind_speed", sierra_wind_speed,
-                   time=3600)
-
-    sierra_expected_snow = memcache.get("sierra_expected_snow")
-    if sierra_expected_snow is None:
-      sierra_expected_snow_query = models.SierraAvyCenterExpectedSnowfall.all()
-      sierra_expected_snow_query.order('-date_time_added')
-      sierra_expected_snow = sierra_expected_snow_query.get()
-      logging.info('sierra_expected_snow: %s' % str(sierra_expected_snow))
-      memcache.set("sierra_expected_snow", sierra_expected_snow,
-                   time=3600)
-
-    yesterday_data = memcache.get("yesterday_data")
-    if yesterday_data is None:
-      yesterday_data_query = models.YesterdaysWeather.all()
-      yesterday_data_query.order('-date_time_added')
-      yesterday_data = yesterday_data_query.get()
-      logging.info('yesterday_data: %s' % str(yesterday_data))
-      memcache.set("yesterday_data", yesterday_data,
-                    time=3600)
 
     snow_fall_graph = memcache.get("snow_fall_graph")
     if snow_fall_graph is None:
--- mmlib/mmutil.py
+++ mmlib/mmutil.py
@@ -4,7 +4,12 @@ __author__ = "Bill Ferrell"
 """Util functions. 
 """
 
+import datetime
 import logging
+
+import models
+from google.appengine.ext import db
+from google.appengine.api import memcache
 
 
 def SnowfallGraphMaker(data):
@@ -23,3 +28,69 @@ def SnowfallGraphMaker(data):
               daily_snowfall_7_8k + closer)
 
   return complete
+
+
+class DataPopulator(object):
+
+  def handle_weather(self, *args, **kwargs):
+    return models.ThreeDayWeatherForecast.all()
+
+  def handle_roads(self, *args, **kwargs):
+    return models.DOTi80RoadConditions.all()
+
+  def handle_avalanche(self, *args, **kwargs):
+    return models.TodaysAvalancheReport.all()
+
+  def handle_alpine_meadows_snow_report(self, *args, **kwargs):
+    return models.AlpineMeadowsSnowReport.all()
+
+  def handle_squaw_valley_snow_report(self, *args, **kwargs):
+    return models.SquawValleySnowReport.all()
+
+  def handle_kirkwood_snow_report(self, *args, **kwargs):
+    return models.KirkwoodSnowReport.all()
+
+  def handle_expected_snowfall(self, *args, **kwargs):
+    return models.ExpectedSnowfall.all()
+
+  def handle_yahoo_weather(self, *args, **kwargs):
+    return models.YahooWeatherForecast.all()
+
+  def handle_current_obs(self, *args, **kwargs):
+    return models.SierraAvyCenterCurrentObservations.all()
+
+  def handle_sierra_weather(self, *args, **kwargs):
+    return models.SierraAvyCenterWeather.all()
+
+  def handle_sierra_temp(self, *args, **kwargs):
+    return models.SierraAvyCenterTemperatures.all()
+
+  def handle_sierra_wind_direction(self, *args, **kwargs):
+    return models.SierraAvyCenterWindDirection.all()
+
+  def handle_sierra_wind_speed(self, *args, **kwargs):
+    return models.SierraAvyCenterWindSpeed.all()
+
+  def handle_sierra_expected_snow(self, *args, **kwargs):
+    return models.SierraAvyCenterExpectedSnowfall.all()
+
+  def handle_yesterday_data(self, *args, **kwargs):
+    return models.YesterdaysWeather.all()
+
+
+  def handle(self, type, *args, **kwargs):
+    data = memcache.get("%s" % type)
+    if data is None:
+      func = getattr(self, 'handle_%s' % type, None)
+      if func is None:
+        raise Exception("No handler for type %r" % type)
+      raw_data = func(*args, **kwargs)
+      raw_data.order('-date_time_added')
+      data = raw_data.get()
+      if data:
+        if data.date_time_added:
+          data.date_time_added = (data.date_time_added - 
+                                  datetime.timedelta(hours=8))
+      memcache.set("%s" % type, data, time=3600)
+    
+    return data
